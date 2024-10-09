96d4c85f31b395da2ae7f3ad49c0bcacf777d79f
clara15 <clara15@externos.bancoestado.cl>
2024-10-09 11:39:12 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const db = require("./db");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+var app = express();
+
+var jsonParser = bodyParser.json();
+
+app.use(cors());
+
+app.get("/blogs", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM blogs");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/blogs", jsonParser, async (req, res) => {
+  try {
+    await db.query(
+      "INSERT INTO blogs(title, subtitle, body, report_type, is_primary, publisher_name, publisher_job) " +
+        "VALUES ($1, $2, $3, $4, $5, $6, $7)",
+      [
+        req.body.title,
+        req.body.subtitle,
+        req.body.body,
+        req.body.report_type,
+        req.body.is_primary,
+        req.body.publisher_name,
+        req.body.publisher_job,
+      ]
+    );
+    res.status(200).json({
+      message: "Blog was inserted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
---
+++ db.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASS,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DB_NAME,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
