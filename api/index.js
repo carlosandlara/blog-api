9366f0e66342f2ff6ecbe505e8a8c489757f3e54
clara15 <clara15@externos.bancoestado.cl>
2024-11-26 10:24:40 -0300

    changes to index file

--- api/index.js
+++ api/index.js
@@ -17,74 +17,74 @@ app.get("/", (req, res) => {
   res.send("Hola");
 });
 
-app.get("/blogs", async (req, res) => {
-  try {
-    const result = await supabase.from("blogs").select();
-    res.json(result);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// app.get("/blogs", async (req, res) => {
+//   try {
+//     const result = await supabase.from("blogs").select();
+//     res.json(result);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-app.get("/blogs/:id", async (req, res) => {
-  try {
-    const result = await supabase
-      .from("blogs")
-      .select()
-      .eq("id", req.params.id);
-    res.json(result);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// app.get("/blogs/:id", async (req, res) => {
+//   try {
+//     const result = await supabase
+//       .from("blogs")
+//       .select()
+//       .eq("id", req.params.id);
+//     res.json(result);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-app.post("/blogs", jsonParser, async (req, res) => {
-  try {
-    const result = await supabase.from("blogs").insert({
-      title: req.body.title,
-      subtitle: req.body.subtitle,
-      body: req.body.body,
-      report_type: req.body.reportType,
-      is_primary: req.body.isPrimary,
-      publisher_name: req.body.publisherName,
-      publisher_job: req.body.publisherJob,
-    });
-    res.status(200).json({
-      message: "Blog was created successfully",
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// app.post("/blogs", jsonParser, async (req, res) => {
+//   try {
+//     const result = await supabase.from("blogs").insert({
+//       title: req.body.title,
+//       subtitle: req.body.subtitle,
+//       body: req.body.body,
+//       report_type: req.body.reportType,
+//       is_primary: req.body.isPrimary,
+//       publisher_name: req.body.publisherName,
+//       publisher_job: req.body.publisherJob,
+//     });
+//     res.status(200).json({
+//       message: "Blog was created successfully",
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-app.put("/blogs/:id", jsonParser, async (req, res) => {
-  try {
-    const result = await supabase
-      .from("blogs")
-      .update({
-        title: req.body.title,
-        subtitle: req.body.subtitle,
-        body: req.body.body,
-        report_type: req.body.reportType,
-        is_primary: req.body.isPrimary,
-        publisher_name: req.body.publisherName,
-        publisher_job: req.body.publisherJob,
-      })
-      .eq("id", req.params.id);
-    res.status(200).json({
-      message: "Blog was updated successfully",
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// app.put("/blogs/:id", jsonParser, async (req, res) => {
+//   try {
+//     const result = await supabase
+//       .from("blogs")
+//       .update({
+//         title: req.body.title,
+//         subtitle: req.body.subtitle,
+//         body: req.body.body,
+//         report_type: req.body.reportType,
+//         is_primary: req.body.isPrimary,
+//         publisher_name: req.body.publisherName,
+//         publisher_job: req.body.publisherJob,
+//       })
+//       .eq("id", req.params.id);
+//     res.status(200).json({
+//       message: "Blog was updated successfully",
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${3000}`);
 });
 
 module.exports = app;
